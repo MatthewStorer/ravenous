f3ee8c0a03b18734178be1991cfcc237a838fbd7
matthew <matthewstorer@outlook.com.au>
2017-08-29 12:49:35 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList propName={businesses} />
+        <BusinessList business={businesses} />
       </div>
     );
   }
