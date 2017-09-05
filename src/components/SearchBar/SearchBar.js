da4e5a0b2bb438a6ad00c225817882086fcd5765
matthew <matthewstorer@outlook.com.au>
2017-09-05 10:43:41 +1000

    updated

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -60,7 +60,7 @@ handleSearch(event) {
   renderSortByOptions() {
   return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-      return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={this.handleSortByChange} > {sortByOption} </li>;
+      return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} > {sortByOption} </li>;
 
       });
 }
---
+++ src/util/Yelp.js
@@ -0,0 +1,58 @@
+const clientId = '_C8dTusVGvODMuygWbZrtg';
+const secret = 'acq7D4ANoJP3OavGiq3DtLAFXQAxJpuRWYK8FMMVwhC2ZGT0d3gtawa3Z3enZRvZ';
+let accessToken;
+
+const Yelp = {
+
+getAccessToken() {
+  if(accessToken) {
+    return new Promise(resolve => {
+      resolve(accessToken)
+    });
+  }
+
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+    {method: 'POST'}).then(response => {
+  return response.json();
+}).then(jsonResponse => {accessToken = jsonResponse.access_token});
+
+
+},
+
+search(term, location, sortBy) {
+
+  return Yelp.getAccessToken().then(() => {
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers:
+        {
+          Authorisation: `Bearer ${accessToken}`}
+        }
+    );
+  }).then(response => {
+    return response.json();
+  }).then(jsonResponse => {
+      if(jsonResponse.businesses)
+
+      {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+}));
+}
+});
+
+}
+
+}
+
+export default Yelp;
